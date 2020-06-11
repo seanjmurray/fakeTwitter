8fa8cfbb886efc2c4dc27d13f09f14e0c5c614b0
Sean Murray <sean.j.murray1@gmail.com>
2020-06-10 23:42:18 -0700

    Add boilerplate in server.js for pg integration

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const express = require('express');
 const pg = require('pg');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
+const DB = process.env.DATABASE_URL;
 
+const client = new pg.Client(DB);
+client.on('error', err => console.error(err));
 
-
-app.listen(PORT, ()=> console.log(`I am alive on ${PORT}`))
+client.connect()
+.then(()=>{
+  app.listen(PORT, ()=> console.log(`I am alive on ${PORT}`))
+})
 
 
